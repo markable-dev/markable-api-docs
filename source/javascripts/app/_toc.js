6382a014699c57b28fc7b55b3f63ad54941d7842
Dicky Lou <louying128@gmail.com>
2017-02-28 10:39:28 -0600

    fixed fonts to be proxima-nova globally (through .js) + fixed logo + top bar

--- source/javascripts/app/_toc.js
+++ source/javascripts/app/_toc.js
@@ -2,6 +2,19 @@
 //= require ../lib/_jquery_ui
 //= require ../lib/_jquery.tocify
 //= require ../lib/_imagesloaded.min
+
+ (function(d) {
+    var config = {
+      kitId: 'ztq7far',
+      scriptTimeout: 3000,
+      async: true
+    },
+    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+  })(document);
+
+//adding Typekit for proxima-nova font to our site
+
+
 (function (global) {
   'use strict';
 
